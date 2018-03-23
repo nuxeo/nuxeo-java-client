6ede79b9f7c8a5b45e75a49e6a8e262a1e1b5db1
Kevin Leturc <kleturc@nuxeo.com>
2018-03-23 15:24:28 +0100

    JAVACLIENT-150: Cleanup & format

--- nuxeo-java-client/src/main/java/org/nuxeo/client/methods/RepositoryAPI.java
+++ nuxeo-java-client/src/main/java/org/nuxeo/client/methods/RepositoryAPI.java
@@ -50,18 +50,20 @@ public interface RepositoryAPI {
     Call<Document> fetchDocumentRoot();
 
     @GET("repo/{repositoryName}/id/{documentId}")
-    Call<Document> fetchDocumentById(@Path("documentId") String documentId, @Path("repositoryName") String repositoryName);
+    Call<Document> fetchDocumentById(@Path("documentId") String documentId,
+            @Path("repositoryName") String repositoryName);
 
     @POST("repo/{repositoryName}/id/{parentId}")
-    Call<Document> createDocumentById(@Path("parentId") String parentId,
-            @Body Document document, @Path("repositoryName") String repositoryName);
+    Call<Document> createDocumentById(@Path("parentId") String parentId, @Body Document document,
+            @Path("repositoryName") String repositoryName);
 
     @PUT("repo/{repositoryName}/id/{documentId}")
-    Call<Document> updateDocument(@Path("documentId") String documentId,
-            @Body Document document, @Path("repositoryName") String repositoryName);
+    Call<Document> updateDocument(@Path("documentId") String documentId, @Body Document document,
+            @Path("repositoryName") String repositoryName);
 
     @DELETE("repo/{repositoryName}/id/{documentId}")
-    Call<ResponseBody> deleteDocument(@Path("documentId") String documentId, @Path("repositoryName") String repositoryName);
+    Call<ResponseBody> deleteDocument(@Path("documentId") String documentId,
+            @Path("repositoryName") String repositoryName);
 
     @GET("id/{documentId}")
     Call<Document> fetchDocumentById(@Path("documentId") String documentId);
@@ -76,16 +78,19 @@ public interface RepositoryAPI {
     Call<ResponseBody> deleteDocument(@Path("documentId") String documentId);
 
     @GET("repo/{repositoryName}/path{docPath}")
-    Call<Document> fetchDocumentByPath(@Path(value = "docPath", encoded = true) String docPath, @Path("repositoryName") String repositoryName);
+    Call<Document> fetchDocumentByPath(@Path(value = "docPath", encoded = true) String docPath,
+            @Path("repositoryName") String repositoryName);
 
     @POST("repo/{repositoryName}/path{parentPath}")
-    Call<Document> createDocumentByPath(@Path("parentPath") String parentPath, @Body Document document, @Path("repositoryName") String repositoryName);
+    Call<Document> createDocumentByPath(@Path("parentPath") String parentPath, @Body Document document,
+            @Path("repositoryName") String repositoryName);
 
     @GET("path{documentPath}")
     Call<Document> fetchDocumentByPath(@Path(value = "documentPath", encoded = true) String docPath);
 
     @POST("path{parentPath}")
-    Call<Document> createDocumentByPath(@Path(value = "parentPath", encoded = true) String parentPath, @Body Document document);
+    Call<Document> createDocumentByPath(@Path(value = "parentPath", encoded = true) String parentPath,
+            @Body Document document);
 
     /* Query */
 
@@ -113,7 +118,8 @@ public interface RepositoryAPI {
     Call<Audit> fetchAuditById(@Path("documentId") String documentId);
 
     @GET("repo/{repositoryName}/path{documentPath}/@audit")
-    Call<Audit> fetchAuditByPath(@Path(value = "documentPath", encoded = true) String documentPath, @Path("repositoryName") String repositoryName);
+    Call<Audit> fetchAuditByPath(@Path(value = "documentPath", encoded = true) String documentPath,
+            @Path("repositoryName") String repositoryName);
 
     @GET("repo/{repositoryName}/id/{documentId}/@audit")
     Call<Audit> fetchAuditById(@Path("documentId") String documentId, @Path("repositoryName") String repositoryName);
@@ -127,10 +133,12 @@ public interface RepositoryAPI {
     Call<ACP> fetchPermissionsById(@Path("documentId") String documentId);
 
     @GET("repo/{repositoryName}/path{documentPath}/@acl")
-    Call<ACP> fetchPermissionsByPath(@Path(value = "documentPath", encoded = true) String documentPath, @Path("repositoryName") String repositoryName);
+    Call<ACP> fetchPermissionsByPath(@Path(value = "documentPath", encoded = true) String documentPath,
+            @Path("repositoryName") String repositoryName);
 
     @GET("repo/{repositoryName}/id/{documentId}/@acl")
-    Call<ACP> fetchPermissionsById(@Path("documentId") String documentId, @Path("repositoryName") String repositoryName);
+    Call<ACP> fetchPermissionsById(@Path("documentId") String documentId,
+            @Path("repositoryName") String repositoryName);
 
     /* Children */
 
@@ -141,7 +149,8 @@ public interface RepositoryAPI {
     Call<Documents> fetchChildrenById(@Path("parentId") String parentId);
 
     @GET("repo/{repositoryName}/path{parentPath}/@children")
-    Call<Documents> fetchChildrenByPath(@Path(value = "parentPath", encoded = true) String parentPath, @Path("repositoryName") String repositoryName);
+    Call<Documents> fetchChildrenByPath(@Path(value = "parentPath", encoded = true) String parentPath,
+            @Path("repositoryName") String repositoryName);
 
     @GET("repo/{repositoryName}/id/{parentId}/@children")
     Call<Documents> fetchChildrenById(@Path("parentId") String parentId, @Path("repositoryName") String repositoryName);
@@ -149,17 +158,20 @@ public interface RepositoryAPI {
     /* Blobs */
 
     @GET("path{documentPath}/@blob/{fieldPath}")
-    Call<FileBlob> fetchBlobByPath(@Path(value = "documentPath", encoded = true) String documentPath, @Path(value = "fieldPath", encoded = true) String fieldPath);
+    Call<FileBlob> fetchBlobByPath(@Path(value = "documentPath", encoded = true) String documentPath,
+            @Path(value = "fieldPath", encoded = true) String fieldPath);
 
     @GET("id/{documentId}/@blob/{fieldPath}")
-    Call<FileBlob> fetchBlobById(@Path("documentId") String documentId, @Path(value = "fieldPath", encoded = true) String fieldPath);
+    Call<FileBlob> fetchBlobById(@Path("documentId") String documentId,
+            @Path(value = "fieldPath", encoded = true) String fieldPath);
 
     @GET("repo/{repositoryName}/path{documentPath}/@blob/{fieldPath}")
-    Call<FileBlob> fetchBlobByPath(@Path(value = "documentPath", encoded = true) String documentPath, @Path(value = "fieldPath", encoded = true) String fieldPath, @Path("repositoryName") String repositoryName);
+    Call<FileBlob> fetchBlobByPath(@Path(value = "documentPath", encoded = true) String documentPath,
+            @Path(value = "fieldPath", encoded = true) String fieldPath, @Path("repositoryName") String repositoryName);
 
     @GET("repo/{repositoryName}/id/{documentId}/@blob/{fieldPath}")
-    Call<FileBlob> fetchBlobById(@Path("documentId") String documentId, @Path(value = "fieldPath", encoded = true) String fieldPath, @Path("repositoryName") String repositoryName);
-
+    Call<FileBlob> fetchBlobById(@Path("documentId") String documentId,
+            @Path(value = "fieldPath", encoded = true) String fieldPath, @Path("repositoryName") String repositoryName);
 
     /* Workflows */
 
@@ -167,25 +179,30 @@ public interface RepositoryAPI {
     Call<Workflow> startWorkflowInstanceWithDocId(@Path("documentId") String documentId, @Body Workflow workflow);
 
     @POST("repo/{repositoryName}/id/{documentId}/@workflow")
-    Call<Workflow> startWorkflowInstanceWithDocId(@Path("documentId") String documentId, @Body Workflow workflow, @Path("repositoryName") String repositoryName);
+    Call<Workflow> startWorkflowInstanceWithDocId(@Path("documentId") String documentId, @Body Workflow workflow,
+            @Path("repositoryName") String repositoryName);
 
     @POST("path{documentPath}/@workflow")
-    Call<Workflow> startWorkflowInstanceWithDocPath(@Path(value = "documentPath", encoded = true) String documentPath, @Body Workflow workflow);
+    Call<Workflow> startWorkflowInstanceWithDocPath(@Path(value = "documentPath", encoded = true) String documentPath,
+            @Body Workflow workflow);
 
     @POST("repo/{repositoryName}/path{documentPath}/@workflow")
-    Call<Workflow> startWorkflowInstanceWithDocPath(@Path(value = "documentPath", encoded = true) String documentPath, @Body Workflow workflow, @Path("repositoryName") String repositoryName);
+    Call<Workflow> startWorkflowInstanceWithDocPath(@Path(value = "documentPath", encoded = true) String documentPath,
+            @Body Workflow workflow, @Path("repositoryName") String repositoryName);
 
     @GET("id/{documentId}/@workflow")
     Call<Workflows> fetchWorkflowInstances(@Path("documentId") String documentId);
 
     @GET("repo/{repositoryName}/id/{documentId}/@workflow")
-    Call<Workflows> fetchWorkflowInstances(@Path("documentId") String documentId, @Path("repositoryName") String repositoryName);
+    Call<Workflows> fetchWorkflowInstances(@Path("documentId") String documentId,
+            @Path("repositoryName") String repositoryName);
 
     @GET("path{documentPath}/@workflow")
     Call<Workflows> fetchWorkflowInstancesByDocPath(@Path(value = "documentPath", encoded = true) String documentPath);
 
     @GET("repo/{repositoryName}/id/{documentId}/@workflow")
-    Call<Workflows> fetchWorkflowInstancesByDocPath(@Path("documentPath") String documentPath, @Path("repositoryName") String repositoryName);
+    Call<Workflows> fetchWorkflowInstancesByDocPath(@Path("documentPath") String documentPath,
+            @Path("repositoryName") String repositoryName);
 
     @GET("workflow/{workflowInstanceId}")
     Call<Workflow> fetchWorkflowInstance(@Path("workflowInstanceId") String workflowInstanceId);
