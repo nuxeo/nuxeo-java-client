a485f241dfdf73bb676b9d4f0d621f40338d46c7
Kevin Leturc <kleturc@nuxeo.com>
2020-08-12 16:01:23 +0200

    JAVACLIENT-202: Allow to pass several queryParams to query endpoint

--- nuxeo-java-client/src/main/java/org/nuxeo/client/methods/RepositoryAPI.java
+++ nuxeo-java-client/src/main/java/org/nuxeo/client/methods/RepositoryAPI.java
@@ -101,11 +101,21 @@ public interface RepositoryAPI {
     @GET("query")
     Call<Documents> query(@Query("query") String query);
 
+    /**
+     * @deprecated since 3.7, use {{@link #query(String, String, String, String, String, String, String...)}} instead
+     */
     @GET("query")
+    @Deprecated
     Call<Documents> query(@Query("query") String query, @Query("pageSize") String pageSize,
             @Query("currentPageIndex") String currentPageIndex, @Query("maxResults") String maxResults,
             @Query("sortBy") String sortBy, @Query("sortOrder") String sortOrder,
             @Query("queryParams") String queryParams);
+
+    @GET("query")
+    Call<Documents> query(@Query("query") String query, @Query("pageSize") String pageSize,
+                          @Query("currentPageIndex") String currentPageIndex, @Query("maxResults") String maxResults,
+                          @Query("sortBy") String sortBy, @Query("sortOrder") String sortOrder,
+                          @Query("queryParams") String... queryParams);
 
     @GET("query/{providerName}")
     Call<Documents> queryByProvider(@Path("providerName") String providerName, @Query("pageSize") String pageSize,
