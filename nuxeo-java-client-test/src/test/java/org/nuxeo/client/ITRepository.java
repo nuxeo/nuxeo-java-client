d971ff7ceacc75af14842be5380e7be95b117b26
Kevin Leturc <kleturc@nuxeo.com>
2020-09-09 11:19:19 +0200

    Update 3.7.1-SNAPSHOT to 3.8.0-SNAPSHOT

--- nuxeo-java-client-test/src/test/java/org/nuxeo/client/ITRepository.java
+++ nuxeo-java-client-test/src/test/java/org/nuxeo/client/ITRepository.java
@@ -57,6 +57,7 @@ import java.util.TimeZone;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
